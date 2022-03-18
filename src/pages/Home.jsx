49872296f6d6859c7f4e5478e8294f2187c1ef29
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-03-18 16:33:34 -0300

    style: starting use of bootstrap in home page - add container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Header from '../components/Header';
 import ListTokens from '../components/ListTokens';
 
 export default function Home() {
   return (
-    <>
+    <Container>
       <Header />
       <ListTokens />
-    </>
+    </Container>
   );
 }
