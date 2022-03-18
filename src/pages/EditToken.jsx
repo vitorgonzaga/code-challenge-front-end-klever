43859e78fa81517947407e5c68bfe8b24683403d
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-03-18 15:00:55 -0300

    feat: create Edit page adding header and EditTokenForm components inside it

---
+++ src/pages/EditToken.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EditTokenForm from '../components/EditTokenForm';
+import Header from '../components/Header';
+
+export default function Edit() {
+  return (
+    <>
+      <Header />
+      <EditTokenForm />
+    </>
+  );
+}
