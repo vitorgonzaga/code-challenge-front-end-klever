d14f64cbac0c17c2e59c90843249710879c7cce6
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-03-18 14:57:49 -0300

    feat: add Header and AddTokenForm components in AddToken page

---
+++ src/pages/AddToken.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddTokenForm from '../components/AddTokenForm';
+import Header from '../components/Header';
+
+export default function AddToken() {
+  return (
+    <>
+      <Header />
+      <AddTokenForm />
+    </>
+  );
+}
