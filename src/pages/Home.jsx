7b474ba4fe2e5728d7f172f61bc480851be40860
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-03-18 14:55:46 -0300

    feat: add Header and ListTokens components into Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+import ListTokens from '../components/ListTokens';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <ListTokens />
+    </>
+  );
+}
